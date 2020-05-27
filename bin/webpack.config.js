4b66fc34552ec5cdaef18cc06a42299097796bfa
Reeyou <ryu993830137@163.com>
2020-05-27 16:12:49 +0800

    chore:update eslint error code

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -1,15 +1,16 @@
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // init index.html
+const HtmlWebpackPlugin = require('html-webpack-plugin') // init index.html
 
-const os = require('os') //获取电脑的处理器有几个核心，作为配置传入
+const os = require('os') // 获取电脑的处理器有几个核心，作为配置传入
 const HappyPack = require('happypack')
+
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
 
 
 module.exports = {
     entry: {
-        app: './src/index.js'
+        app: './src/index.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -18,15 +19,15 @@ module.exports = {
             inject: true,
             minify: {
                 collapseWhitespace: true,
-            }
+            },
         }),
         new webpack.NamedModulesPlugin(), // 查看修补的依赖
         new webpack.HashedModuleIdsPlugin(),
-        new HappyPack({ //开启多线程打包
+        new HappyPack({ // 开启多线程打包
             id: 'happy-babel-js',
             loaders: ['babel-loader?cacheDirectory=true'],
-            threadPool: happyThreadPool
-        })
+            threadPool: happyThreadPool,
+        }),
     ],
     // 通用模块代码分离
     optimization: {
@@ -44,50 +45,50 @@ module.exports = {
                     test: /node_modules/,
                     chunks: 'initial',
                     priority: -10,
-                    enforce: true
+                    enforce: true,
                 },
                 styles: {
                     name: 'styles',
                     test: /(\.less|\.css)$/,
                     chunks: 'all',
                     enforce: true,
-                }
-            }
-        }
+                },
+            },
+        },
     },
     output: {
         filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: '/',
     },
     module: {
         rules: [
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'file-loader'
+                    'file-loader',
                 ],
             },
             {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
                 ],
             },
             {
                 test: /\.scss$/,
                 use: [
-                    "css-loader",
-                    "sass-loader"
-                ]
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
-                    'file-loader'
+                    'file-loader',
                 ],
-            }
-        ]
-    }
-};
+            },
+        ],
+    },
+}
