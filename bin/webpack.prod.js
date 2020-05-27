46e1b34f106cb20093c3fc4dbede59e85aff63af
Reeyou <ryu993830137@163.com>
2020-05-27 16:17:20 +0800

    chore:update eslint error code

--- bin/webpack.prod.js
+++ bin/webpack.prod.js
@@ -1,11 +1,12 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.config.js');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // clear dist/
-const ExtractTextPlugin = require("extract-text-webpack-plugin"); // 抽离css，不再内嵌于js bundle
+/* eslint-disable import/no-extraneous-dependencies */
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin') // clear dist/
+// const ExtractTextPlugin = require('extract-text-webpack-plugin') // 抽离css，不再内嵌于js bundle
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin') // 优化压缩css资源
-const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin') // html-webpack-plugin扩展插件
+// const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin') // html-webpack-plugin扩展插件
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer') // 构架bundle 分析
+const baseConfig = require('./webpack.config.js')
 
 const config = require('../config/webpack')
 
@@ -18,19 +19,19 @@ const webpackProdConfig = merge(baseConfig, {
         //     filename: utils.assetsPath('css/[name].[hash:8].css'),
         // }),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         new OptimizeCSSPlugin({
             cssProcessorOptions: {
                 safe: true,
             },
         }),
-    ]
-});
+    ],
+})
 
 // npm run build --report
 if (config.build.bundleAnalyzerReport) {
-    webpackProdConfig.plugins.push(new BundleAnalyzerPlugin());
+    webpackProdConfig.plugins.push(new BundleAnalyzerPlugin())
 }
 
 module.exports = webpackProdConfig
